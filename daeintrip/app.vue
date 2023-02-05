a49d4aa20ba8f67606347c53497d2900a7e9c43f
dnfwlxo11 <dnfwlxo11@naver.com>
2023-02-05 23:45:10 +0900

    add: first commit

---
+++ daeintrip/app.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="app">
+    <HomePage />
+  </div>
+</template>
+
+<script>
+import HomePage from './pages';
+
+export default {
+  name: 'App',
+  components: {
+    HomePage
+  },
+  setup() {
+
+  }
+}  
+</script>
---
+++ daeintrip/pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        Hello, I'm daein
+    </div>
+</template>
+
+<script>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
